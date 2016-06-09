a8a7b4f510bac9ee357adbd39ce4a28a5721d87d
Ben Quach <benquach16@yahoo.com>
2016-06-09 01:33:08 -0700

    get events working properly with arrow functions

--- keylistener.ts
+++ keylistener.ts
@@ -2,14 +2,14 @@
 
 class KeyListener
 {
-	static m_leftMouseButton: boolean;
-	static m_rightMouseButton: boolean;
+	m_leftMouseButton: boolean;
+	m_rightMouseButton: boolean;
 	m_mouseX: number;
 	m_mouseY: number;
 	constructor()
 	{
-		KeyListener.m_leftMouseButton = false;
-		KeyListener.m_rightMouseButton = false;
+		this.m_leftMouseButton = false;
+		this.m_rightMouseButton = false;
 		var canvas = document.getElementById('content');
 
 		canvas.addEventListener("mousedown", this.mouseDown, false);
@@ -17,41 +17,42 @@ class KeyListener
 
 	}
 
-	mouseDown(event)
+	mouseDown = (event) =>
 	{
 
 		if(event.which==3)
 		{
 			console.log("right click");
-			KeyListener.m_rightMouseButton = true;
+			this.m_rightMouseButton = true;
 		}
 		else if(event.which==1)
 		{
 			console.log("left click");
-			KeyListener.m_leftMouseButton = true;
+			this.m_leftMouseButton = true;
 		}
 	}
 
-	mouseUp(event)
+	mouseUp = (event) =>
 	{
 		console.log("button released");
 		if(event.which==3)
 		{
-			KeyListener.m_rightMouseButton = false;
+			this.m_rightMouseButton = false;
 		}
 		else if(event.which ==1)
 		{
-			KeyListener.m_leftMouseButton = false;
+			this.m_leftMouseButton = false;
 		}
 	}
 
 	getLeftMouseButtonDown() : boolean
 	{
-		return KeyListener.m_leftMouseButton;
+		return this.m_leftMouseButton;
 	}
 
 	getRightMouseButtonDown() : boolean
 	{
-		return KeyListener.m_rightMouseButton;
+		console.log(this.m_rightMouseButton);
+		return this.m_rightMouseButton;
 	}
 }

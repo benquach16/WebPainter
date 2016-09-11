b2778c7b42bc3a74607de2ec39a28c0caa6f813d
Ben Quach <benquach16@yahoo.com>
2016-09-10 17:21:30 -0700

    add more lighting code

--- shaders/fs_model.ts
+++ shaders/fs_model.ts
@@ -3,19 +3,23 @@ var FS_SHADER_SOURCE=`
 varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
+
+varying vec3 worldSpaceLightPos;
+
 uniform sampler2D tex;
 
-vec3 lightPos = vec3(2.0, 0.0, 10.0);
 
 void main()
 {
     //apply phong shader
-    vec3 lightDirection = normalize(vPosition - lightPos);
+    vec3 lightDirection = normalize(vPosition - worldSpaceLightPos);
 
-	vec4 color = texture2D(tex, vUv);
-    color = color * max(dot(vNormal, lightDirection), 0.0);
-
-    gl_FragColor = color;
+    vec4 texColor = texture2D(tex, vUv);  
+	vec4 color;
+    vec4 ambient = vec4(0.2, 0.2, 0.2, 1.0);
+    ambient = ambient * texColor;
+    color = texColor * max(dot(vNormal, lightDirection), 0.0);
+    gl_FragColor = color + ambient;
 
 }
 `;
--- shaders/vs_model.ts
+++ shaders/vs_model.ts
@@ -4,12 +4,18 @@ varying vec3 vNormal;
 varying vec3 vPosition;
 varying vec2 vUv;
 
+varying vec3 worldSpaceLightPos;
+
+vec3 lightPos = vec3(2.0, -250.0, -20.0);
 
 void main()
 {
     vUv = uv; 
     vPosition = position;
     vNormal = normalize(normal);
+
+    worldSpaceLightPos = lightPos;
+
 	gl_Position = projectionMatrix *
                 modelViewMatrix *
                 vec4(position,1.0);

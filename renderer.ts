c51e42ca42e522c04dd6fba0d3f64b831fb5d05b
Ben Quach <benquach16@yahoo.com>
2016-09-17 14:11:51 -0700

    add new camera orbit code

--- renderer.ts
+++ renderer.ts
@@ -18,6 +18,9 @@ class Renderer
 
 	m_mouseOldX: number;
 	m_mouseOldY: number;
+	m_cameraDistance: number;
+	m_angleX: number;
+	m_angleY: number;
 
 	m_screenWidth: number;
 	m_screenHeight: number;
@@ -29,6 +32,9 @@ class Renderer
 		this.m_keylistener = new KeyListener();
 		this.m_mouseOldX = 0;
 		this.m_mouseOldY = 0;
+		this.m_angleX = 180;
+		this.m_angleY = 20;
+		this.m_cameraDistance = 150;
 
 				
 		this.m_renderer = new THREE.WebGLRenderer();
@@ -53,12 +59,25 @@ class Renderer
 		
 		this.m_camera.lookAt(new THREE.Vector3(0,0,0));
 
-		this.m_dummy.add(this.m_camera);
+		this.m_scene.add(this.m_camera);
 		console.log(this.m_dummy.rotation);
 		var light = new THREE.AmbientLight( 0x404040 ); // soft white light
 		this.m_scene.add( light );
 		this.m_terrain = new Mesh(this.m_scene);
 
+	}
+
+	//redesign me!
+	orbitCamera() : void
+	{
+		var temp = Math.cos(this.m_angleY*Math.PI/180)*this.m_cameraDistance;
+		var X = Math.sin(this.m_angleX*Math.PI/180)*temp;
+		var Y = Math.sin(this.m_angleY*Math.PI/180)*this.m_cameraDistance;
+		var  Z = Math.cos(this.m_angleX*Math.PI/180)*temp;
+		this.m_camera.position.x = X;
+		this.m_camera.position.y = Y;
+		this.m_camera.position.z = Z;
+		this.m_camera.lookAt(new THREE.Vector3(0,0,0));
 	}
 
 	run(): void
@@ -68,23 +87,27 @@ class Renderer
 		RenderScene.getInstance().renderToTexture(this.m_renderer);
 		
 		//this.m_terrain.render(this.m_renderer, this.m_scene, this.m_camera);
-		//if we have right click rotate the camera around the origin
-		
+
+		this.orbitCamera();
 		var mouseX:number = this.m_keylistener.getMouseX();
 		var mouseY:number = this.m_keylistener.getMouseY();
+		//if we have right click rotate the camera around the origin
 		if(this.m_keylistener.getRightMouseButtonDown())
  		{
-
-			//rotate camera around origin
 			if(this.m_mouseOldX != mouseX)
 			{
-				//turn this into radians
-				this.m_dummy.rotation.x -= Math.PI * (this.m_mouseOldX - mouseX)/360;
-				this.m_dummy.rotation.z -= Math.PI * (this.m_mouseOldY - mouseY)/360;
-				//this.m_dummy.rotation.y+=0.1;
+				this.m_angleX += (this.m_mouseOldX - mouseX);
+				if (this.m_angleX >= 360) this.m_angleX -= 360;
+				else if (this.m_angleX < 0) this.m_angleX += 360;
 				this.m_mouseOldX = mouseX;
-				this.m_mouseOldY = mouseY;
-				//console.log(this.m_dummy.rotation);				
+			}
+
+			if(this.m_mouseOldY != mouseY)
+			{
+				this.m_angleY += (this.m_mouseOldY - mouseY);
+				if (this.m_angleY >= 360) this.m_angleY -= 360;
+				else if (this.m_angleY < 0) this.m_angleY += 360;
+				this.m_mouseOldY = mouseY;				
 			}
 			
 		}
@@ -96,11 +119,9 @@ class Renderer
 		}
 		if(this.m_keylistener.getMouseWheel() != 0)
 		{
-			//console.log(this.m_keylistener.getMouseWheel());
-			this.m_camera.position.y += this.m_keylistener.getMouseWheel();
+			this.m_cameraDistance += this.m_keylistener.getMouseWheel();
 			//see keylistener class
 			this.m_keylistener.resetMouseWheel();
-
 		}
 
 		if(this.m_keylistener.getLeftMouseButtonDown())
